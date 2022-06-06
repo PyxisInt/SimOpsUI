ec3aefd6b7d1b7d0a5529720d5f5da5338ef81b4
Prabal Ghosh <prabalg@gmail.com>
2022-06-06 18:31:18 -0400

    Added Auth0 login

---
+++ SimOpsUI/Auth0Client.cs
@@ -0,0 +1,18 @@
+﻿using Auth0.OidcClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimOpsUI
+{
+    public class Auth0Client : Auth0ClientBase
+    {
+        public Auth0Client(Auth0ClientOptions options)
+            : base(options, "winforms")
+        {
+            options.Browser = options.Browser ?? new WebViewBrowserChromium();
+        }
+    }
+}
--- SimOpsUI/Form1.cs
+++ SimOpsUI/Form1.cs
@@ -1,3 +1,5 @@
+using Auth0.OidcClient;
+
 namespace SimOpsUI
 {
     public partial class Form1 : Form
@@ -5,6 +7,20 @@ namespace SimOpsUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new Auth0Client(new Auth0ClientOptions { 
+                Domain = "dev-rvdj99qb.us.auth0.com",
+                ClientId = "nN6A30JHTcNWpviapTpj5DIsSbQDnYYb",
+                Browser = new WebViewBrowserChromium()
+            });
+            var loginResult = await client.LoginAsync(new { audience = "https://simopsservice.herokuapp.com/" });
+            if (loginResult.IsError)
+            {
+                MessageBox.Show("Failed to login with Auth0!");
+            }
         }
     }
 }
